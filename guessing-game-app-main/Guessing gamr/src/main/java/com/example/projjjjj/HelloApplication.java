18307ee323a052db0443120bfa3c1c2c42424869
Shehan121 <shehannimsara470@gmail.com>
2026-04-01 12:08:03 +0200

    Add files via upload

---
+++ guessing-game-app-main/Guessing gamr/src/main/java/com/example/projjjjj/HelloApplication.java
@@ -0,0 +1,32 @@
+package com.example.projjjjj;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage)  {
+try{
+Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+Scene scene= new Scene(root);
+stage.setScene(scene);
+stage.show();
+} catch (Exception e) {
+e.printStackTrace();
+}
+
+
+}
+public static void main ( String[] args) {
+launch(args);
+}
+}
